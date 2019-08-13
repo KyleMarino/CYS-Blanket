dd4d58f986d4ff1fecb67b521a052d10fbabc9a5
marino.kyle@gmail.com <marino.kyle@gmail.com>
2019-08-13 13:31:05 -0400

    What the fuck?

---
+++ CYS-Blanket/UserControls/NewSalesorderItem.Designer.cs
@@ -0,0 +1,140 @@
+﻿namespace CYS_Blanket.UserControls
+{
+    partial class NewSalesorderItem
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.dtExpiration = new System.Windows.Forms.DateTimePicker();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtPO = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtSO = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.btnSave);
+            this.groupBox1.Controls.Add(this.dtExpiration);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.txtPO);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.txtSO);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(3, 0);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(949, 76);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "New Salesorder";
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(849, 30);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(75, 23);
+            this.btnSave.TabIndex = 7;
+            this.btnSave.Text = "Save";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // dtExpiration
+            // 
+            this.dtExpiration.Location = new System.Drawing.Point(411, 30);
+            this.dtExpiration.Name = "dtExpiration";
+            this.dtExpiration.Size = new System.Drawing.Size(200, 20);
+            this.dtExpiration.TabIndex = 6;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(323, 33);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(82, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Expiration Date:";
+            // 
+            // txtPO
+            // 
+            this.txtPO.Location = new System.Drawing.Point(203, 30);
+            this.txtPO.Name = "txtPO";
+            this.txtPO.Size = new System.Drawing.Size(100, 20);
+            this.txtPO.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(168, 33);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(29, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "PO#";
+            // 
+            // txtSO
+            // 
+            this.txtSO.Location = new System.Drawing.Point(50, 30);
+            this.txtSO.Name = "txtSO";
+            this.txtSO.Size = new System.Drawing.Size(100, 20);
+            this.txtSO.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(15, 33);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(29, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "SO#";
+            // 
+            // NewSalesorderItem
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.groupBox1);
+            this.Name = "NewSalesorderItem";
+            this.Size = new System.Drawing.Size(956, 83);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.DateTimePicker dtExpiration;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtPO;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtSO;
+        private System.Windows.Forms.Label label1;
+    }
+}
